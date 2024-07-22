585de560969578da63cf43168393d5d2075877f8
ghina2111 <ghinapuspamurti@gmail.com>
2024-07-23 00:29:29 +0700

    upgrade

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['index.html'],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#38bdf8',
+        secondary: '#94a3b8',
+        dark: '#1e1b4b',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
   },
   plugins: [],
 }
