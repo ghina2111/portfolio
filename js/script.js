fc1b82a98e3b301a577e66d51ea9329731dbc39e
ghina2111 <ghinapuspamurti@gmail.com>
2024-08-18 20:18:49 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,9 +1,32 @@
+// //navbar fixed
+// window.onscroll = function() {
+//     const header = document.querySelector('header');
+//     const fixedNav = header.offsetTop;
+
+//     if(window.scrollY > fixedNav) {
+//         header.classList.add('navbar-fixed');
+//     } else {
+//         header.classList.remove('navbar-fixed');
+//     }
+// };
+
+// // hamburger
+// const hamburger = document.querySelector('#hamburger');
+// const navMenu = document.querySelector('#nav-menu');
+
+// hamburger.addEventListener('click', function() {
+//     hamburger.classList.toggle('hamburger-active');
+//     navMenu.classList.toggle('hidden');
+// })
+
+
+
 //navbar fixed
 window.onscroll = function() {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
 
-    if(window.pageYOffset > fixedNav) {
+    if(window.scrollY > fixedNav) {
         header.classList.add('navbar-fixed');
     } else {
         header.classList.remove('navbar-fixed');
@@ -12,7 +35,9 @@ window.onscroll = function() {
 
 // hamburger
 const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
 
 hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('hamburger-active');
-})
+    navMenu.classList.toggle('hidden');
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,25 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ['index.html'],
+//   theme: {
+//     container: {
+//       center: true,
+//       padding: '16px',
+//     },
+//     extend: {
+//       colors: {
+//         primary: '#38bdf8',
+//         secondary: '#94a3b8',
+//         dark: '#1e1b4b',
+//       },
+//       screens: {
+//         '2xl': '1320px',
+//       },
+//     },
+//   },
+//   plugins: [],
+// }
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['index.html'],
@@ -15,8 +37,10 @@ module.exports = {
       screens: {
         '2xl': '1320px',
       },
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'], // Menambahkan font Roboto
+      },
     },
   },
   plugins: [],
 }
-
